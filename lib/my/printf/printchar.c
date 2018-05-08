f16eb1854c21f57ab9541f3bf709314b1039d351
kadadot216 <yoctopolochoon54@gmail.com>
2018-05-08 09:25:08 +0200

    Updating before merging

---
+++ lib/my/printf/printchar.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** printchar.c
+** File description:
+** Print function for my_printf
+*/
+
+#include "my_put.h"
+#include "my_printf.h"
+
+void	printchar(int fd, va_list ap)
+{
+	my_putchar_fd(fd, (char) va_arg(ap, int));
+}
